c7b3cc1860ddd4c0db387803df3b492ce8d6cac7
Eric Bottard <ebottard@pivotal.io>
2019-04-08 11:38:58 +0200

    Sprinkle some more OOO

--- src/main/java/io/projectriff/processor/Processor.java
+++ src/main/java/io/projectriff/processor/Processor.java
@@ -2,7 +2,6 @@ package io.projectriff.processor;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.Arrays;
 
 import com.github.bsideup.liiklus.protocol.Assignment;
 import com.github.bsideup.liiklus.protocol.PublishRequest;
@@ -11,6 +10,8 @@ import com.github.bsideup.liiklus.protocol.ReceiveReply;
 import com.github.bsideup.liiklus.protocol.ReceiveRequest;
 import com.github.bsideup.liiklus.protocol.SubscribeReply;
 import com.github.bsideup.liiklus.protocol.SubscribeRequest;
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
@@ -22,37 +23,27 @@ import io.rsocket.RSocketFactory;
 import io.rsocket.transport.netty.client.WebsocketClientTransport;
 import reactor.core.publisher.Flux;
 
-public class EntryPoint {
+public class Processor {
 
-	private static String group;
+	private final Flux<String> input;
 
-	private static String output;
+	private String group;
 
-	private static RiffClient riffStub;
+	private String output;
+
+	private RiffClient riffStub;
+
+	private ReactorLiiklusServiceGrpc.ReactorLiiklusServiceStub liiklus;
 
 	public static void main(String[] args) throws IOException {
 
 		var gwAddress = System.getenv("GATEWAY");
-		group = System.getenv("GROUP");
-		var sInputs = System.getenv("INPUTS");
-		var sOutputs = System.getenv("OUTPUTS");
-
 		gwAddress = "localhost:6565";
 		gwAddress = "35.204.31.102:6565";
-		group = "processor0";
-		sInputs = "numbers";
-		sOutputs = "squares,cubes";
-		output = "squares";
-
-		var inputs = Flux.fromArray(sInputs.split(","));
-		var outputs = Flux.fromArray(sOutputs.split(","));
-
 		var channel = NettyChannelBuilder.forTarget(gwAddress)
 				.directExecutor()
 				.usePlaintext()
 				.build();
-		var liiklusStub = ReactorLiiklusServiceGrpc.newReactorStub(channel);
-
 
 		WebsocketClientTransport websocketClientTransport = WebsocketClientTransport
 				.create(URI.create("ws://kmprssr2.default.35.241.251.246.nip.io/ws"));
@@ -62,71 +53,89 @@ public class EntryPoint {
 				.start()
 				.block();
 
+		var group = System.getenv("GROUP");
+		group = "processor0";
 
-		riffStub = new RiffClient(rSocket);
-		var input = Flux.just(sInputs);
+		var sInputs = System.getenv("INPUTS");
+		var sOutputs = System.getenv("OUTPUTS");
+		sInputs = "numbers";
+		sOutputs = "squares";
+
+		var inputs = Flux.fromArray(sInputs.split(","));
+		var outputs = Flux.fromArray(sOutputs.split(","));
+
+		Processor processor = new Processor(channel, rSocket, group, sInputs, sOutputs);
+		processor.run();
+
+	}
+
+	public Processor(Channel channel, RSocket rSocket, String group, String sInputs, String sOutputs) {
+		this.liiklus = ReactorLiiklusServiceGrpc.newReactorStub(channel);
+		this.riffStub = new RiffClient(rSocket);
+		this.group = group;
+		this.input = Flux.just(sInputs);
+		this.output = sOutputs;
+
+	}
+
+	public void run() throws IOException {
+
 
 		/*
-		inputs.map(EntryPoint::subscribeRequestForInput)
-				.flatMap(liiklusStub::subscribe)
-				.filter(EntryPoint::isAssignment)
-				.map(SubscribeReply::getAssignment)
-				.map(EntryPoint::receiveRequestForAssignment)
-				.flatMap(liiklusStub::receive)
-				.map(EntryPoint::toRiffMessage)
-				.compose(EntryPoint::riffWindowing)
-				.doOnEach(f -> )
+		 * inputs.map(Processor::subscribeRequestForInput) .flatMap(liiklusStub::subscribe)
+		 * .filter(Processor::isAssignment) .map(SubscribeReply::getAssignment)
+		 * .map(Processor::receiveRequestForAssignment) .flatMap(liiklusStub::receive)
+		 * .map(Processor::toRiffMessage) .compose(Processor::riffWindowing) .doOnEach(f -> )
+		 * 
+		 * ;
+		 */
 
-		;
-		*/
-
-		input.map(EntryPoint::subscribeRequestForInput)
-				.flatMap(liiklusStub::subscribe)
+		input.map(this::subscribeRequestForInput)
+				.flatMap(liiklus::subscribe)
 				.log("A")
-				.filter(EntryPoint::isAssignment)
+				.filter(this::isAssignment)
 				.log("B")
 				.map(SubscribeReply::getAssignment)
 				.log("C")
-				.map(EntryPoint::receiveRequestForAssignment)
+				.map(this::receiveRequestForAssignment)
 				.log("D")
-				.flatMap(liiklusStub::receive)
+				.flatMap(liiklus::receive)
 				.log("E")
-				.map(EntryPoint::toRiffMessage)
+				.map(this::toRiffMessage)
 				.log("F")
-				.compose(EntryPoint::riffWindowing)
+				.compose(this::riffWindowing)
 				.log("G")
-				.map(EntryPoint::invoke)
+				.map(this::invoke)
 				.log("H")
-				.concatMap(f -> f.concatMap(m -> liiklusStub.publish(createPR(m))))
+				.concatMap(f -> f.concatMap(m -> liiklus.publish(createPR(m))))
 				.log("I")
 				.subscribe();
 
 		System.in.read();
 
-
 	}
 
-	private static Flux<Message> invoke(Flux<Message> in) {
+	private  Flux<Message> invoke(Flux<Message> in) {
 		ByteBuf metadata = ByteBufUtil.writeUtf8(ByteBufAllocator.DEFAULT, "application/json");
 		return riffStub.invoke(in, metadata);
 	}
 
-	private static PublishRequest createPR(Message m) {
+	private  PublishRequest createPR(Message m) {
 		return PublishRequest.newBuilder()
 				.setValue(m.getPayload())
 				.setTopic(output)
 				.build();
 	}
 
-	private static ReceiveRequest receiveRequestForAssignment(Assignment assignment) {
+	private  ReceiveRequest receiveRequestForAssignment(Assignment assignment) {
 		return ReceiveRequest.newBuilder().setAssignment(assignment).build();
 	}
 
-	private static <T> Flux<Flux<T>> riffWindowing(Flux<T> linear) {
+	private  <T> Flux<Flux<T>> riffWindowing(Flux<T> linear) {
 		return linear.window(30);
 	}
 
-	private static Message toRiffMessage(ReceiveReply receiveReply) {
+	private  Message toRiffMessage(ReceiveReply receiveReply) {
 		// TODO: somehow propagate which input (index) this came from
 		return Message.newBuilder()
 				.setPayload(receiveReply.getRecord().getValue())
@@ -134,11 +143,11 @@ public class EntryPoint {
 				.build();
 	}
 
-	private static boolean isAssignment(SubscribeReply reply) {
+	private  boolean isAssignment(SubscribeReply reply) {
 		return reply.getReplyCase() == SubscribeReply.ReplyCase.ASSIGNMENT;
 	}
 
-	private static SubscribeRequest subscribeRequestForInput(String i) {
+	private  SubscribeRequest subscribeRequestForInput(String i) {
 		return SubscribeRequest.newBuilder()
 				.setTopic(i)
 				.setGroup(group)
